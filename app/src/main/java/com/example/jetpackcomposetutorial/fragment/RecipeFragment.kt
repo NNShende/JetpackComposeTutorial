24b84addecb6717f42255e737702b75058da08b6
Nikhil Shende <nkhlshnd99@gmail.com>
2021-10-30 16:12:58 -0400

    Navigation setup

--- app/src/main/java/com/example/jetpackcomposetutorial/fragment/RecipeFragment.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/fragment/RecipeFragment.kt
@@ -5,9 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
@@ -15,10 +13,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.example.jetpackcomposetutorial.R
 
-class RecipeListFragment : Fragment() {
+class RecipeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,19 +24,11 @@ class RecipeListFragment : Fragment() {
             setContent {
                 Column(modifier = Modifier.padding(16.dp)) {
                     Text(
-                        text = "Recipe List",
+                        text = "Recipe Fragment",
                         style = TextStyle(
-                            fontSize = TextUnit.Sp(24)
+                            fontSize = TextUnit.Sp(21)
                         )
                     )
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(
-                        onClick = {
-                            findNavController().navigate(R.id.view_recipe)
-                        },
-                    ) {
-                        Text(text = "To Recipe Fragment")
-                    }
                 }
             }
         }
