a16fdf9ed14280652d1bab2d2dbe4f6b81597f0d
Nikhil Shende <nkhlshnd99@gmail.com>
2021-10-30 15:13:24 -0400

    Fragment Setup

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -1,84 +1,16 @@
 package com.example.jetpackcomposetutorial
 
 import android.os.Bundle
-import android.widget.Toast
-import android.widget.Toast.LENGTH_LONG
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.imageFromResource
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ScrollableColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        color = Color(0xFFF2F2F2)
-                    )
-            ) {
-                Image(
-                    bitmap = imageFromResource(
-                        res = resources,
-                        resId = R.drawable.happymeal
-                    ),
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop
-                )
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "Happy Meal",
-                            style = TextStyle(
-                                fontSize = TextUnit.Companion.Sp(26)
-                            )
-                        )
-                        Text(
-                            text = "$5.99",
-                            style = TextStyle(
-                                color = Color(0xFF85BB65),
-                                fontSize = TextUnit.Companion.Sp(17)
-                            ),
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    }
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Text(
-                        text = "800 Calories",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(17)
-                        )
-                    )
-                    Spacer(modifier = Modifier.padding(10.dp))
+        setContentView(R.layout.activity_main)
 
-                    Button(
-                        onClick = {
-                            Toast.makeText(this@MainActivity, "Ordered", LENGTH_LONG).show()
-                        },
-                        modifier = Modifier.align(Alignment.CenterHorizontally)
-                    ) {
-                        Text(text = "Order")
-                    }
-                }
-            }
-        }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, RecipeListFragment())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/jetpackcomposetutorial/RecipeListFragment.kt
@@ -0,0 +1,51 @@
+package com.example.jetpackcomposetutorial
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.fragment.app.Fragment
+import com.example.jetpackcomposetutorial.view.HorizontalDottedProgress
+
+class RecipeListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Text(text = "This is a Title", color = Color.Blue, textAlign = TextAlign.Center)
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .border(1.dp, Color.Black),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "THIS IS A COMPOSABLE INSIDE THE FRAGMENT")
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    CircularProgressIndicator()
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Text(text = "Neat")
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    val customView = HorizontalDottedProgress(AmbientContext.current)
+                    AndroidView(viewBlock = {customView})
+                }
+            }
+        }
+    }
+}
