9a6dd5afcebeb6f51a7b0ee8948e6ea032d77561
Nikhil Shende <nkhlshnd99@gmail.com>
2021-10-31 00:06:22 -0400

    Persisting Text Field

--- app/src/main/java/com/example/jetpackcomposetutorial/presentation/ui/recipe_list/RecipeListFragment.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/presentation/ui/recipe_list/RecipeListFragment.kt
@@ -5,8 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.TextField
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.jetpackcomposetutorial.presentation.components.RecipeCard
@@ -28,16 +35,25 @@ class RecipeListFragment: Fragment() {
             setContent {
                 val recipes = viewModel.recipes.value
 
-                LazyColumn{
-                    itemsIndexed(
-                        items = recipes
-                    ){ index, recipe ->
-                        RecipeCard(recipe = recipe, onClick = {})
+                val query = viewModel.query.value
+
+                Column() {
+                    TextField(
+                        value = query,
+                        onValueChange = { newValue ->
+                            viewModel.onQueryChanged(newValue)
+                        }
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    LazyColumn{
+                        itemsIndexed(
+                            items = recipes
+                        ){ index, recipe ->
+                            RecipeCard(recipe = recipe, onClick = {})
+                        }
                     }
                 }
-
             }
         }
     }
-
 }
